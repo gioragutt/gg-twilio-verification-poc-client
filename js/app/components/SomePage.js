3cba5d953af22faddd91cb9f80fe356c129b7512
Giora Guttsait <giora111@gmail.com>
2017-12-06 23:57:17 +0200

    cleaned the 'home page'

--- js/app/components/SomePage.js
+++ js/app/components/SomePage.js
@@ -1,34 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//@flow
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default () => (
   <View style={styles.container}>
     <Text style={styles.welcome}>
-      Welcome to React Native!
+      YOU ARE VERIFIED!!!
     </Text>
     <Text style={styles.instructions}>
-      To get started, edit App.js
-    </Text>
-    <Text style={styles.instructions}>
-      {instructions}
+      How do you feel about that?
     </Text>
   </View>
 )
@@ -44,10 +25,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
