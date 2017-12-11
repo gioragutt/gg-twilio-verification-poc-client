e422c60b97bfd8bf8734ae931eb85018899364d1
Giora Guttsait <giora111@gmail.com>
2017-12-12 00:45:09 +0200

    Change production backend url

--- js/shared/services/config.js
+++ js/shared/services/config.js
@@ -1,3 +1,6 @@
-const defaultApiBaseUrl = 'http://10.0.2.2:3000/api/v1'
+const defaultApiBaseUrl =
+process.env.NODE_ENV === 'production'
+  ? 'https://foody-poc-server.herokuapp.com/'
+  : 'http://10.0.2.2:3000/api/v1'
 
 export const apiBaseUrl = process.env.API_BASE_URL || defaultApiBaseUrl
