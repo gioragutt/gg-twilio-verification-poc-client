4c9ab7cf51c763a9d29a147a1a37056de695b92c
Giora Guttsait <giora111@gmail.com>
2017-12-13 21:44:13 +0200

    Fix base url to production server

--- js/shared/services/config.js
+++ js/shared/services/config.js
@@ -1,6 +1,6 @@
 const defaultApiBaseUrl =
 process.env.NODE_ENV === 'production'
-  ? 'https://foody-poc-server.herokuapp.com/'
+  ? 'https://foody-poc-server.herokuapp.com/api/v1'
   : 'http://10.0.2.2:3000/api/v1'
 
 export const apiBaseUrl = process.env.API_BASE_URL || defaultApiBaseUrl
